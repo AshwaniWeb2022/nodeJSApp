40958d7199abdba6b858e6129f7619c616227333
Ashwani <ashwani.kumar@codmsoftware.com>
2024-04-16 13:06:33 +0530

    m,m,m,

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express =  require('express');
 const app = express();
-const path= require('path')
+// const path= require('path')
 const PORT = process.env.PORT || 8000
 
 app.set('view engine', 'hbs');
 
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', 'views');
 // app.use(express.static(__dirname + '/public'));
 app.get('/',(req,res) =>{
     res.render('index');
